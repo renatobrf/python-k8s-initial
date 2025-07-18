3312a0cf0cceec4b027d1716b0d8eb0d590d8bda
Renato Ponce Barufi <Renato.Barufi@ibm.com>
2025-07-18 13:04:03 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
-# app.py
-import random  # Importa o módulo random
-import time    # Importa o módulo time
+import random
+import time
 
 def rolldice():
     return random.randint(1, 6)  # Retorna um número aleatório entre 1 e 6
 
+def welcome_message():
+    print("Bem-vindo ao projeto Python K8s! Esta aplicação simula o lançamento de um dado.")
+
 print("Hello from Kubernetes! v0.1.1")
+welcome_message()
 
 while True:
     time.sleep(10)  # Pausa de 10 segundos        
