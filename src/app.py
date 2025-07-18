332afb65843c360361f275b1be0657f635db43b8
Renato Ponce Barufi <Renato.Barufi@ibm.com>
2025-07-18 13:50:26 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import random
 import time
+from datetime import datetime
 
 def rolldice():
     return random.randint(1, 6)  # Retorna um número aleatório entre 1 e 6
@@ -11,5 +12,7 @@ print("Hello from Kubernetes! v0.1.1")
 welcome_message()
 
 while True:
-    time.sleep(10)  # Pausa de 10 segundos        
-    print(f"Rolando o dado... Resultado: {rolldice()}")
+    time.sleep(10)  # Pausa de 10 segundos
+    now = datetime.now()
+    formatted = now.strftime("%Y-%m-%d %H-%M-%S")        
+    print(f"Rolando o dado {formatted}... Resultado: {rolldice()}")
