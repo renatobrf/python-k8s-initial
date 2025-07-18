f1cf56cc30253b24aabacd7236b665f967b1b93f
Renato Ponce Barufi <Renato.Barufi@ibm.com>
2025-07-18 09:32:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 # app.py
-print("Hello from Kubernetes!")
+import random  # Importa o módulo random
+import time    # Importa o módulo time
+
+def rolldice():
+    return random.randint(1, 6)  # Retorna um número aleatório entre 1 e 6
+
+print("Hello from Kubernetes! v0.1.1")
+
+while True:
+    time.sleep(10)  # Pausa de 10 segundos        
+    print(f"Rolando o dado... Resultado: {rolldice()}")
